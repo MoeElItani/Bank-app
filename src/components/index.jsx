49899a3ea777443a7eb7bd14f25ae26f29cc0de2
MoeElItani <mhmd98itani@gmail.com>
2022-11-09 19:53:45 +0200

    create navbar

---
+++ src/components/index.jsx
@@ -0,0 +1,29 @@
+import Button from './Button'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import GetStarted from './GetStarted'
+import Stats from './Stats'
+import Business from './Business'
+import Billing from './Billing'
+import CardDeal from './CardDeal'
+import Testimonials from './Testimonials'
+import FeedbackCard from './FeedbackCard'
+import Clients from './Clients'
+import CTA from './CTA'
+import Footer from './Footer'
+
+export {
+   Button,
+   Hero,
+   Navbar,
+   GetStarted,
+   Stats,
+   Business,
+   Billing,
+   CardDeal,
+   Testimonials,
+   FeedbackCard,
+   Clients,
+   CTA,
+   Footer,
+}
