51f8a457454e26c9b35ed20a0f00c3110502e815
MoeElItani <mhmd98itani@gmail.com>
2022-11-12 15:40:00 +0200

    create CTA Component and edit Clients Style

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -14,7 +14,7 @@ const Clients = () => (
                <img
                   src={client.logo}
                   alt={client.logo}
-                  className='sm:w-[192px] w-[100px] object-contain '
+                  className='sm:w-[192px] w-[100px] object-contain client'
                />
             </div>
          ))}
